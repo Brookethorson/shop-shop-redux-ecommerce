2c176bb7d945741214843b6ff84760d45ebef6db
Thorson <thorsonb@nala.roche.com>
2022-01-06 13:41:11 -0800

    create and update shopping cart

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,23 +1,30 @@
 import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers';
+import { useProductReducer } from "./reducers";
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
-      products: [],
-      categories: [],
-      currentCategory: '',
-    });
-    // use this to confirm it works!
-    console.log(state);
-    return <Provider value={[state, dispatch]} {...props} />;
-  };
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    categories: [],
+    currentCategory: "",
+  });
+  // use this to confirm it works!
+  console.log(state);
+  return <Provider value={[state, dispatch]} {...props} />;
+};
 
 const useStoreContext = () => {
-    return useContext(StoreContext);
-  };
+  return useContext(StoreContext);
+};
 
+// const [state, dispatch] = useProductReducer({
+//   products: [],
+//   cart: [],
+//   cartOpen: false,
+//   categories: [],
+//   currentCategory: "",
+// });
 
 export { StoreProvider, useStoreContext };
